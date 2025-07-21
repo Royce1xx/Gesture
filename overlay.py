304cc14caafa3122ba8a69ce45adad6ad00266f7
Royce <roycekinchloe@gmail.com>
2025-07-21 04:46:48 -0400

    Next Track is working
    Previous track is popping up but it isn't working the functionality isnt working to well

---
+++ overlay.py
@@ -0,0 +1,14 @@
+import cv2
+
+def draw_overlay(frame, gesture):
+    overlays = {
+        "volume_up":    (" Volume Up", (0, 255, 0)), # Green
+        "play_pause":   (" Play/Pause", (0, 255, 255)), # Yellow
+        "mute":         (" Mute", (0, 0, 255)), # Red
+        "screenshot":   (" Screenshot", (255, 0, 0)), # Blue
+        "next_track":   (" Next Track", (128, 0, 128)),  # Purple
+        "previous_track":("Previous Track",(255,165,0))
+    }
+    if gesture in overlays:
+        text, color = overlays[gesture]
+        cv2.putText(frame, text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 3)
